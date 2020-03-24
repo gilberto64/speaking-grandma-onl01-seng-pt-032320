76ac341e27f7f682cd12d9c0f74f19b82ef8eaff
Gilberto Ocampo <ocampo@gate.net>
2020-03-24 16:27:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,7 @@ require "pry"
 def speak_to_grandma (phrase)
   if phrase == phrase.upcase
     puts "NO, NOT SINCE 1938"
-    binding.pry 
-    
+  elseif phrase! = 
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end 
